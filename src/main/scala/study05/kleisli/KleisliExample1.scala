8d2b335f3a5043f17f4e26432b78f3057ada4bda
dysangelist <darya.tarasova@colorado.edu>
2018-11-06 18:57:09 +0900

    :hammer: function composition was backwards

--- src/main/scala/study05/kleisli/KleisliExample1.scala
+++ src/main/scala/study05/kleisli/KleisliExample1.scala
@@ -4,8 +4,8 @@ object KleisliExample1 extends App {
   val add2 = (i: Int) => i + 2
   val times3 = (i: Int) => i * 2
 
-  val add2AndTimes3 = add2.andThen(times3) // f(g(x)) g ⚪︎ f
-  val times3AndAdd2 = add2.compose(times3) // g(f(x)) f ⚪︎ g
+  val add2AndTimes3 = add2.andThen(times3) // f(g(x)) f ⚪︎ g
+  val times3AndAdd2 = add2.compose(times3) // g(f(x)) g ⚪︎ f
 
   print(add2AndTimes3(2))
 }
